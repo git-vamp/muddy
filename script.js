58021af4cb77ee367b556cdd770396f19c5c6dfa
personGithubAccount <astefard@gmail.com>
2022-06-16 20:29:13 +0500

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let yes = document.querySelector('#Yes')
+let no = document.querySelector('#No')
+
+no.addEventListener('click', () => {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+yes.addEventListener('click', () => {
+    window.location.href = "https://www.youtube.com/watch?v=attUrDwfdr8";
+})
